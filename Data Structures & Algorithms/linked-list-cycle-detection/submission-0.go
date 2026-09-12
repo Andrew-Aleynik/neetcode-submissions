5af56712ec90a3fd5531c142bd760ad9e7047b13
Andrew-Aleynik <84306935+Andrew-Aleynik@users.noreply.github.com>
2026-09-12 21:55:44 +0300

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.go
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ 
+func hasCycle(head *ListNode) bool {
+	i, j := head, head
+	for i != nil && j != nil && j.Next != nil {
+		i = i.Next
+		j = j.Next.Next
+		if i == j {
+			return true
+		}
+	}
+	return false
+}
