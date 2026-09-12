1c60b2b5ab04bd5b10101b2fd16b7b0e2fae4c95
Andrew-Aleynik <84306935+Andrew-Aleynik@users.noreply.github.com>
2026-09-12 20:34:55 +0300

    Add: three-integer-sum - submission-8

---
+++ Data Structures & Algorithms/three-integer-sum/submission-8.go
@@ -0,0 +1,28 @@
+func threeSum(nums []int) [][]int {
+	answer := make([][]int, 0)
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		for j, k := i+1, len(nums)-1; j < k; {
+			sum := nums[i] + nums[j] + nums[k]
+			if sum == 0 {
+				answer = append(answer, []int{nums[i], nums[j], nums[k]})
+				for ;j < k && nums[j] == nums[j+1]; {
+					j++
+				}
+				for ;j < k && nums[k] == nums[k-1]; {
+					k--
+				}
+				j++
+				k--
+			} else if sum < 0 {
+				j++
+			} else {
+				k--
+			}
+		}
+	}
+	return answer
+}
